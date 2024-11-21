3a950e5420b4daf1642aab3a7ebb2fa2b7798dab
Damivk <damianpemo@gmail.com>
2024-11-21 16:40:47 +0100

    creamos el recyclerView, en el layout activity_main, creamos el item para recycler, y lo añadimos al MainActivity

--- app/src/main/java/com/example/milista/activities/MainActivity.kt
+++ app/src/main/java/com/example/milista/activities/MainActivity.kt
@@ -6,13 +6,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.milista.R
+import com.example.milista.adapters.TaskAdapter
 import com.example.milista.data.providers.TaskDAO
 import com.example.milista.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    lateinit var adapter: TaskAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +32,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         /*val taskDAO = TaskDAO(this)
         //taskDAO.insert(Task(-1, "Limpiar el coche", false))
